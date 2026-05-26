de476fa407cd982f4f7bee98dae3881f838bba52
cityisempty <154956996+cityisempty@users.noreply.github.com>
2026-05-26 13:16:59 +0800

    添加 electron.vite.config.ts

---
+++ electron.vite.config.ts
@@ -0,0 +1,37 @@
+import { resolve } from 'path'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve('src/main/index.ts')
+        }
+      }
+    }
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve('src/preload/index.ts')
+        }
+      }
+    }
+  },
+  renderer: {
+    plugins: [vue()],
+    root: resolve('src/renderer'),
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve('src/renderer/index.html')
+        }
+      }
+    }
+  }
+})
